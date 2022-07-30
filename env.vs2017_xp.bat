a2302ec3d87b3fb46a06e140ca496f0744be7639
ywxt <ywxtcwh@qq.com>
2022-07-30 23:53:01 +0800

    change arch to win64

--- env.vs2017_xp.bat
+++ env.vs2017_xp.bat
@@ -6,7 +6,7 @@ rem REQUIRED: path to Boost source directory
 if not defined BOOST_ROOT set BOOST_ROOT=%RIME_ROOT%\deps\boost_1_69_0
 
 rem OPTIONAL: architecture, Visual Studio version and platform toolset
-set ARCH=Win32
+set ARCH=Win64
 set BJAM_TOOLSET=msvc-14.1
 set CMAKE_GENERATOR="Visual Studio 15 2017"
 set PLATFORM_TOOLSET=v141_xp
