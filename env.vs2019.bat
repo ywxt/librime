ca9e23edaada46b2431d50cf4ae018c6f9b673b1
ywxt <ywxtcwh@qq.com>
2022-07-31 00:12:49 +0800

    change arch to x64

--- env.vs2019.bat
+++ env.vs2019.bat
@@ -6,7 +6,7 @@ rem REQUIRED: path to Boost source directory
 if not defined BOOST_ROOT set BOOST_ROOT=%RIME_ROOT%\deps\boost_1_76_0
 
 rem OPTIONAL: architecture, Visual Studio version and platform toolset
-set ARCH=Win64
+set ARCH=x64
 set BJAM_TOOLSET=msvc-14.2
 set CMAKE_GENERATOR="Visual Studio 16 2019"
 set PLATFORM_TOOLSET=v142
